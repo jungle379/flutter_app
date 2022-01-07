3df5325789ff21bc2d965d15c6ff077529cac7b3
jungle379 <63592531+jungle379@users.noreply.github.com>
2022-01-07 12:00:49 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'BottomNavBar Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,6 +36,10 @@ class MainPage extends StatelessWidget {
               RaisedButton(onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) {
                 return SubPage1();
               })), child: new Text('Subページ_2へ'),),
+              Text('Bottombuttonへ',style: TextStyle(fontWeight: FontWeight.bold,fontSize:20)),
+              RaisedButton(onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return MyStatefulWidget();
+              })), child: new Text('BottomButtonへ')),
               Container(
                 margin: EdgeInsets.all(24),
                 child:Text('Test', style: TextStyle(fontWeight: FontWeight.bold,fontSize:32,color: Colors.red)),
@@ -82,7 +90,7 @@ class SubPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Navigator'),
+        title: new Text('Navigator',style: TextStyle(color: Colors.yellow),),
       ),
       body: new Container(
         color: Colors.lightBlue,
@@ -104,7 +112,7 @@ class SubPage1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Navigator'),
+        title: new Text('Navigator',style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),),
       ),
       body: new Container(
         color: Colors.lightGreen,
@@ -149,3 +157,90 @@ class SubPage1 extends StatelessWidget {
     );
   }
 }
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('BottomNavigationBar Sample'),
+    ),
+    body: Center(
+      child: Text(
+        "indexNum: $_selectedIndex",
+        style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    ),
+    bottomNavigationBar: BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.book),
+          activeIcon: Icon(Icons.book_online),
+          label: 'Book',
+          tooltip: "This is a Book Page",
+          backgroundColor: Colors.blue,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.business),
+          activeIcon: Icon(Icons.business_center),
+          label: 'Business',
+          tooltip: "This is a Business Page",
+          backgroundColor: Colors.green,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.school),
+          activeIcon: Icon(Icons.school_outlined),
+          label: 'School',
+          tooltip: "This is a School Page",
+          backgroundColor: Colors.purple,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          activeIcon: Icon(Icons.settings_accessibility),
+          label: 'Settings',
+          tooltip: "This is a Settings Page",
+          backgroundColor: Colors.pink,
+        ),
+      ],
+
+      type: BottomNavigationBarType.shifting,
+      // ここで色を設定していても、shiftingにしているので
+      // Itemの方のbackgroundColorが勝ちます。
+      backgroundColor: Colors.red,
+      enableFeedback: true,
+      // IconTheme系統の値が優先されます。
+      iconSize: 18,
+      // 横向きレイアウトは省略します。
+      // landscapeLayout: 省略
+      selectedFontSize: 20,
+      selectedIconTheme: const IconThemeData(size: 30, color: Colors.green),
+      selectedLabelStyle: const TextStyle(color: Colors.red),
+      // ちなみに、LabelStyleとItemColorの両方を選択した場合、ItemColorが勝ちます。
+      selectedItemColor: Colors.black,
+      unselectedFontSize: 15,
+      unselectedIconTheme: const IconThemeData(size: 25, color: Colors.white),
+      unselectedLabelStyle: const TextStyle(color: Colors.purple),
+      // IconTheme系統の値が優先されるのでこの値は適応されません。
+      unselectedItemColor: Colors.red,
+    ),
+  );
+}
+}
+
