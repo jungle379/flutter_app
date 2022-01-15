e706206d50087382271990f9a1f98aebbce89e25
jungle379 <63592531+jungle379@users.noreply.github.com>
2022-01-15 19:38:30 +0900

    syuusei

--- lib/main.dart
+++ lib/main.dart
@@ -50,35 +50,35 @@ class MainPage extends StatelessWidget {
               //   margin: EdgeInsets.all(24),
               //   child: const Text('Test2',style: TextStyle(fontSize: 32,color: Colors.lightBlue)),
               // ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                height: 100,
-                width: 1000,
-                color: Colors.pink,
-                margin: const EdgeInsets.fromLTRB(20,20,20,20),
-                padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
-                child: const Text('Container 1',style: TextStyle(color: Colors.black),),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: 1000,
+                    color: Colors.pink,
+                    margin: const EdgeInsets.fromLTRB(20,20,20,20),
+                    padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+                    child: const Text('Container 1',style: TextStyle(color: Colors.black),),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 1000,
+                    color: Colors.cyan,
+                    margin: const EdgeInsets.fromLTRB(20,20,20,20),
+                    padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+                    child: const Text('Container 2',style: TextStyle(color: Colors.yellow),),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 1000,
+                    color: Colors.green,
+                    margin: const EdgeInsets.fromLTRB(20,20,20,20),
+                    padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+                    child: const Text('Container 3',style: TextStyle(color: Colors.red),),
+                  ),
+                ],
               ),
-              Container(
-                height: 100,
-                width: 1000,
-                color: Colors.cyan,
-                margin: const EdgeInsets.fromLTRB(20,20,20,20),
-                padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
-                child: const Text('Container 2',style: TextStyle(color: Colors.yellow),),
-              ),
-              Container(
-                height: 100,
-                width: 1000,
-                color: Colors.green,
-                margin: const EdgeInsets.fromLTRB(20,20,20,20),
-                padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
-                child: const Text('Container 3',style: TextStyle(color: Colors.red),),
-              ),
-        ],
-      ),
             ],
           ),
         ),
@@ -132,31 +132,43 @@ class SubPage1 extends StatelessWidget {
                   children: <Widget>[
                     Row(
                       children: <Widget>[
-                      Container(
-                        height: 100,
-                        width: 350,
-                        color: Colors.grey.shade200,
-                        margin: const EdgeInsets.fromLTRB(50,20,20,20),
-                        padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
-                        child: const Text('Grey.shade200',style: TextStyle(color: Colors.black),),
-                      ),
-                      Container(
-                        height: 100,
-                        width: 350,
-                        color: Colors.grey.shade500,
-                        margin: const EdgeInsets.fromLTRB(50,20,20,20),
-                        padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
-                        child: const Text('Grey.shade500',style: TextStyle(color: Colors.yellow),),
-                      ),
-                        ],
-                      ),
+                        Container(
+                          height: 100,
+                          width: 200,
+                          color: Colors.grey.shade200,
+                          margin: const EdgeInsets.fromLTRB(50,20,20,20),
+                          padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+                          child: const Text('Grey.shade200',style: TextStyle(color: Colors.black),),
+                        ),
+                        Container(
+                          height: 100,
+                          width: 200,
+                          color: Colors.grey.shade500,
+                          margin: const EdgeInsets.fromLTRB(50,20,20,20),
+                          padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+                          child: const Text('Grey.shade500',style: TextStyle(color: Colors.yellow),),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget> [
                     Container(
                       height: 100,
-                      width: 1000,
-                      color: Colors.grey.shade800,
-                      margin: const EdgeInsets.fromLTRB(20,20,20,20),
+                      width: 200,
+                      color: Colors.grey.shade600,
+                      margin: const EdgeInsets.fromLTRB(50,20,20,20),
                       padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
-                      child: const Text('Grey.shade800',style: TextStyle(color: Colors.red),),
+                      child: const Text('Grey.shade600',style: TextStyle(color: Colors.red),),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 200,
+                      color: Colors.grey.shade800,
+                      margin: const EdgeInsets.fromLTRB(50,20,20,20),
+                      padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+                      child: const Text('Grey.shade800',style: TextStyle(color: Colors.blue),),
+                    ),
+                        ],
                     ),
                   ],
                 ),
@@ -184,72 +196,72 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     });
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('BottomNavigationBar Sample'),
-    ),
-    body: Center(
-      child: Text(
-        "indexNum: $_selectedIndex",
-        style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
       ),
-    ),
-    bottomNavigationBar: BottomNavigationBar(
-      currentIndex: _selectedIndex,
-      onTap: _onItemTapped,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.book),
-          activeIcon: Icon(Icons.book_online),
-          label: 'Book',
-          tooltip: "This is a Book Page",
-          backgroundColor: Colors.blue,
+      body: Center(
+        child: Text(
+          "indexNum: $_selectedIndex",
+          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.business),
-          activeIcon: Icon(Icons.business_center),
-          label: 'Business',
-          tooltip: "This is a Business Page",
-          backgroundColor: Colors.green,
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.school),
-          activeIcon: Icon(Icons.school_outlined),
-          label: 'School',
-          tooltip: "This is a School Page",
-          backgroundColor: Colors.purple,
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          activeIcon: Icon(Icons.settings_accessibility),
-          label: 'Settings',
-          tooltip: "This is a Settings Page",
-          backgroundColor: Colors.pink,
-        ),
-      ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            activeIcon: Icon(Icons.book_online),
+            label: 'Book',
+            tooltip: "This is a Book Page",
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            activeIcon: Icon(Icons.business_center),
+            label: 'Business',
+            tooltip: "This is a Business Page",
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            activeIcon: Icon(Icons.school_outlined),
+            label: 'School',
+            tooltip: "This is a School Page",
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            activeIcon: Icon(Icons.settings_accessibility),
+            label: 'Settings',
+            tooltip: "This is a Settings Page",
+            backgroundColor: Colors.pink,
+          ),
+        ],
 
-      type: BottomNavigationBarType.shifting,
-      // ここで色を設定していても、shiftingにしているので
-      // Itemの方のbackgroundColorが勝ちます。
-      backgroundColor: Colors.red,
-      enableFeedback: true,
-      // IconTheme系統の値が優先されます。
-      iconSize: 18,
-      // 横向きレイアウトは省略します。
-      // landscapeLayout: 省略
-      selectedFontSize: 20,
-      selectedIconTheme: const IconThemeData(size: 30, color: Colors.green),
-      selectedLabelStyle: const TextStyle(color: Colors.red),
-      // ちなみに、LabelStyleとItemColorの両方を選択した場合、ItemColorが勝ちます。
-      selectedItemColor: Colors.black,
-      unselectedFontSize: 15,
-      unselectedIconTheme: const IconThemeData(size: 25, color: Colors.white),
-      unselectedLabelStyle: const TextStyle(color: Colors.purple),
-      // IconTheme系統の値が優先されるのでこの値は適応されません。
-      unselectedItemColor: Colors.red,
-    ),
-  );
-}
+        type: BottomNavigationBarType.shifting,
+        // ここで色を設定していても、shiftingにしているので
+        // Itemの方のbackgroundColorが勝ちます。
+        backgroundColor: Colors.red,
+        enableFeedback: true,
+        // IconTheme系統の値が優先されます。
+        iconSize: 18,
+        // 横向きレイアウトは省略します。
+        // landscapeLayout: 省略
+        selectedFontSize: 20,
+        selectedIconTheme: const IconThemeData(size: 30, color: Colors.green),
+        selectedLabelStyle: const TextStyle(color: Colors.red),
+        // ちなみに、LabelStyleとItemColorの両方を選択した場合、ItemColorが勝ちます。
+        selectedItemColor: Colors.black,
+        unselectedFontSize: 15,
+        unselectedIconTheme: const IconThemeData(size: 25, color: Colors.white),
+        unselectedLabelStyle: const TextStyle(color: Colors.purple),
+        // IconTheme系統の値が優先されるのでこの値は適応されません。
+        unselectedItemColor: Colors.red,
+      ),
+    );
+  }
 }
