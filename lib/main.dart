ade58bed3c0217213aa5ee648698c21a8840caae
jungle379 <63592531+jungle379@users.noreply.github.com>
2022-01-16 21:19:11 +0900

    syuusei

--- lib/main.dart
+++ lib/main.dart
@@ -127,7 +127,6 @@ class SubPage1 extends StatelessWidget {
           child: Column(
               children: <Widget> [
                 const Text('Sub2'),
-                ElevatedButton(onPressed: () => Navigator.of(context).pop(), child: const Text('トップへ戻る')),
                 Column(mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
                     Row(
@@ -170,11 +169,74 @@ class SubPage1 extends StatelessWidget {
                     ),
                         ],
                     ),
+                    Container(
+                      height: 100,
+                      width: 1000,
+                      color: Colors.pink.shade300,
+                      margin: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                      padding: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+                      child: const Text('pink.shade400'),
+                    )
                   ],
                 ),
+                ElevatedButton(onPressed: () => Navigator.of(context).pop(), child: const Text('トップへ戻る')),
               ]
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // currentIndex: _selectedIndex,
+        // onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            activeIcon: Icon(Icons.book_online),
+            label: 'Book',
+            tooltip: "This is a Book Page",
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            activeIcon: Icon(Icons.business_center),
+            label: 'Business',
+            tooltip: "This is a Business Page",
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            activeIcon: Icon(Icons.school_outlined),
+            label: 'School',
+            tooltip: "This is a School Page",
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            activeIcon: Icon(Icons.settings_accessibility),
+            label: 'Settings',
+            tooltip: "This is a Settings Page",
+            backgroundColor: Colors.pink,
+          ),
+        ],
+
+        type: BottomNavigationBarType.shifting,
+        // ここで色を設定していても、shiftingにしているので
+        // Itemの方のbackgroundColorが勝ちます。
+        backgroundColor: Colors.red,
+        enableFeedback: true,
+        // IconTheme系統の値が優先されます。
+        iconSize: 18,
+        // 横向きレイアウトは省略します。
+        // landscapeLayout: 省略
+        selectedFontSize: 20,
+        selectedIconTheme: const IconThemeData(size: 30, color: Colors.green),
+        selectedLabelStyle: const TextStyle(color: Colors.red),
+        // ちなみに、LabelStyleとItemColorの両方を選択した場合、ItemColorが勝ちます。
+        selectedItemColor: Colors.black,
+        unselectedFontSize: 15,
+        unselectedIconTheme: const IconThemeData(size: 25, color: Colors.white),
+        unselectedLabelStyle: const TextStyle(color: Colors.purple),
+        // IconTheme系統の値が優先されるのでこの値は適応されません。
+        unselectedItemColor: Colors.red,
       ),
     );
   }
